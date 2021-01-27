10445b30bc1e671360f78acaed5400c5e7e539d3
刹那芳华life <306862600@qq.com>
2021-01-27 15:22:37 +0800

    添加排序

--- uniCloud-aliyun/cloudfunctions/api/models/cover.js
+++ uniCloud-aliyun/cloudfunctions/api/models/cover.js
@@ -2,7 +2,7 @@ const db = uniCloud.database();
 
 var cover = {
     list: async () => {
-		let list = await db.collection('cover').get();
+		let list = await db.collection('cover').orderBy("num", "desc").get();
 		return list.data
 	},
 	coupons: async () => {
